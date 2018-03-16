32f627615bc46eb904381efbf5727f077b8f40ec
vpsa23 <31572508+vpsa23@users.noreply.github.com>
2018-03-16 10:49:40 -0300

    Delete index.js

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-
-export default createStore(combineReducers({
-  routing: routerReducer,
-  })
-);
